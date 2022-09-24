b1b3e57801aaeda5d1fdf1d2fe72fa90e04fde10
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-09-24 14:19:14 +0200

    Add files via upload

---
+++ Houswork3/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 2 ");
+        System.out.println("2) Количство школьников с именем переменноЙ noNo " +
+                "делят яблоки с переменной koLo поровну,");
+        System.out.println("неделящийся остаток остается в корзинке. ");
+        System.out.println("Сколько яблок останется в корзинке в итоге? ");
+  int koNo = 14;
+  int noNo = 3;
+        System.out.println("В итоге- " + koNo%noNo);
+    }
+
+}
