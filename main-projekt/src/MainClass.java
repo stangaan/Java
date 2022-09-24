97670b519a41b0a75958749a0b86f1c6592bc7ee
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-09-24 14:32:50 +0200

    Lessonswork 20,9,22

---
+++ main-projekt/src/MainClass.java
@@ -0,0 +1,15 @@
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Зарплата Пети");
+        System.out.println("100 рублей");
+        int pet = 100;
+        System.out.println("Зарплата была поднята на 50 рублей");
+        System.out.println(pet+50 + " рублей");
+        pet = 150;
+        System.out.println("В кризис зарплата упала в двое");
+        pet = (pet/2);
+        System.out.println(pet + " рублей");
+        System.out.println("настоящая зарплата Пети");
+        System.out.println(pet + " рублей");
+
+    }}
