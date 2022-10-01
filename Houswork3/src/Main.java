df298683012973332b326ec21ec7e24e3f31f139
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-01 17:07:40 +0200

    Update Main.java

--- Houswork3/src/Main.java
+++ Houswork3/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
                 "делят яблоки с переменной koLo поровну,");
         System.out.println("неделящийся остаток остается в корзинке. ");
         System.out.println("Сколько яблок останется в корзинке в итоге? ");
-  int koNo = 14;
-  int noNo = 3;
+  int appls = 14;
+  int thiechers = 3;
         System.out.println("В итоге- " + koNo%noNo);
     }
 
