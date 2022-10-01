fe54ab1030e457faa742afb4b6d4777d19069849
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-01 18:19:41 +0300

    Домашка с изменением чисел с помощью переменных220922

---
+++ HoumWork2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int number =100;
+        System.out.print("The next number for the number:" + number);
+        number = number+1;
+        System.out.println("is " + number +".");
+        number = number-1;
+        System.out.print("The next number for the number:" + number);
+        number = number-1;
+        System.out.println("is " +number+ ". ");
+    }
+}
