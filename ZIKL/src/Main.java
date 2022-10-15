64bdccbaf1daeac03b891148a2c0dd23a4ae9e7e
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-15 17:29:15 +0100

    с концовкой

--- ZIKL/src/Main.java
+++ ZIKL/src/Main.java
@@ -6,38 +6,44 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         //
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Ввидите пожалуйста после знака = ваш ответ");
-        int number= 1;
-        int points = 5; //пункты за правильный ответ//
+
+        int number = 1;//Здесь можно так же менят с какой цифры начнуться примеры//
+
+        int points = 4; //пункты за правильный ответ//
         while (number <= 100) {
-            sleep(2000);
-            int levels = number * 5; //ввидите математический знак + - * / и любое число с которым вы хотите тренироваться//
-            System.out.print(number + " * 5 = ");//измените значения со знаком и числом//
-            int c = scanner.nextInt();
+            int levels = number * 6; //ввидите математический знак + - * / и любое число с которым вы хотите тренироваться//
+            System.out.print(number + " * 6" +
+                    " = ");//повторите значения со знаком и числом//
+
+            int c = scanner.nextInt();//здесь вводиться ответ в консоли//
             if (c == levels) {
-                System.out.print("Правильно вы получаете!-");
+                System.out.print("Правильно вы получаете! ");
                 while (points <= 100) {
-                    sleep(2000);
-
-                    System.out.println(points + 1+ " пунктов");
                     sleep(1000);
-                    break;
-                }
 
+                    System.out.println(points + 1 + " пунктов");
+                    sleep(1000);
+                    break; //этот процес нужно остановить иначе программа не будет переходить
+                    //на следующий пример//
+                }
                 System.out.println("Вы гений!");
-                points++;
+                points++;//это увеличивает пункты с каждым правильным ответом.//
 
             } else {
                 System.out.print("К сожелению это не правильно. Правельный ответ = ");
                 sleep(2000);
                 System.out.println(levels);
-
             }
-            number++;
-
+            if (number ==100) {
+                while (levels == 600)
+                    System.out.println("Вы действительно математический гений, переходите к другому уровню.");
+                sleep(2000);
+            }
+                number++;
         }
 
+    }}
 
-    }
 
-}
