7cc74f2582d97e23b583a80d64026cef67c43a9b
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-15 17:41:22 +0100

    Add files via upload

--- ZIKL/src/Main.java
+++ ZIKL/src/Main.java
@@ -9,20 +9,18 @@ public class Main {
 
         System.out.println("Ввидите пожалуйста после знака = ваш ответ");
 
-        int number = 1;//Здесь можно так же менят с какой цифры начнуться примеры//
-
+        int number = 99;//Здесь можно так же менят с какой цифры начнуться примеры//
         int points = 4; //пункты за правильный ответ//
         while (number <= 100) {
             int levels = number * 6; //ввидите математический знак + - * / и любое число с которым вы хотите тренироваться//
             System.out.print(number + " * 6" +
                     " = ");//повторите значения со знаком и числом//
 
-            int c = scanner.nextInt();//здесь вводиться ответ в консоли//
-            if (c == levels) {
+            int answer = scanner.nextInt();//здесь вводиться ответ в консоли//
+            if (answer == levels) {
                 System.out.print("Правильно вы получаете! ");
-                while (points <= 100) {
+                while  (points <= 100) {
                     sleep(1000);
-
                     System.out.println(points + 1 + " пунктов");
                     sleep(1000);
                     break; //этот процес нужно остановить иначе программа не будет переходить
@@ -30,16 +28,14 @@ public class Main {
                 }
                 System.out.println("Вы гений!");
                 points++;//это увеличивает пункты с каждым правильным ответом.//
-
-            } else {
+            }
+            else {
                 System.out.print("К сожелению это не правильно. Правельный ответ = ");
                 sleep(2000);
                 System.out.println(levels);
             }
-            if (number ==100) {
-                while (levels == 600)
+            if (levels ==600) {
                     System.out.println("Вы действительно математический гений, переходите к другому уровню.");
-                sleep(2000);
             }
                 number++;
         }
