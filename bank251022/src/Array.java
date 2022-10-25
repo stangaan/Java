0b831b0cfe170c62f562b41767532ec0f08877aa
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-25 22:20:41 +0200

    ОПИСАНИЕ МАССИВА

--- bank251022/src/Array.java
+++ bank251022/src/Array.java
@@ -2,11 +2,12 @@ import java.util.Arrays;
 
 public class Array {
     public static void main(String[] args) throws InterruptedException {
-        int [] array = new int[10];
-        for (int i =5; i<10; i++) {
-            array[i] = 5+i;
+        int [] array = new int[10];//int [] -массив, array-название массива,
+        // new int [10]-количество чисел в массиве
+        for (int i =5; i<10; i++) {//работа с числами в массиве лучше делать с циклом for//
+            array[i] = 5+i;//это это цикл с массивом
         }
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));//так выводиться массив//
 
 
         }
