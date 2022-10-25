59056011a39b95960e317d62fc4c340450655ab5
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-25 12:20:22 +0200

    level1

---
+++ bank251022/src/Main.java
@@ -0,0 +1,16 @@
+import java.time.Year;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner =new Scanner(System.in);
+        int number = scanner.nextInt();
+        int i;
+    for (i=0; i<number; i++){
+        System.out.println(i+1);
+
+        }
+    }
+
+
+    }
