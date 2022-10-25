030baf1d226c166139e4d3ee89a4d2181d188830
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-25 22:19:12 +0200

    МАССИВЫ И РАБОТА С НИМИ

---
+++ bank251022/src/Array.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) throws InterruptedException {
+        int [] array = new int[10];
+        for (int i =5; i<10; i++) {
+            array[i] = 5+i;
+        }
+        System.out.println(Arrays.toString(array));
+
+
+        }
+    }
+
+
+
