a5c8f7be1c0db743bbbdb25053f2c001db4a8d83
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-25 08:22:00 +0200

    Домашка "Ошибок не обнаружено!"

---
+++ Houmwork2510222/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int Number = scanner.nextInt();
+        if (Number==0){
+            System.out.println("Ошибок не обнаружено!");
+        }
+        else {
+            System.out.println("Error!");
+            System.out.println("Error!");
+            System.out.println("Error!");
+
+        }
+
+
+    }
+}
