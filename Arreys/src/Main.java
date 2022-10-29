91758ea235ad9511ad916156e11cbf3caa40f368
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-29 21:50:45 +0200

    изменеие чисел в массиве с помощью "for"

--- Arreys/src/Main.java
+++ Arreys/src/Main.java
@@ -5,13 +5,21 @@ public class Main {
         int [] arrays = new  int[100];//так пишется массив int [] new int [... ]число массивов
         int [] myArrays = new int[20];
         int [] myTwoArrays = new int[50];
-        int [] yearArrays = new int[20]
+        int [] yearArrays = new int[19];
         System.out.println(Arrays.toString(arrays));
     arrays[1] = 10000;//изминение конкретного массива= [ 1] на что меняем конкретный массив= 10000
     myArrays[2] = 365;
     myTwoArrays[49] =322;
+
+
         System.out.println(Arrays.toString(myArrays));//так мы выводим массив в консоль.
+        for (int i = 1; i<100; i+=2) {//изменение массива с помощью цикла for//
+            arrays[i] = i;//как менять числа в масиве в общем//
+        }
+
+
         System.out.println(Arrays.toString(arrays));
+
         System.out.println(Arrays.toString(myTwoArrays));
     }
 }
