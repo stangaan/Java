f8f01a8f0197366eaee4d90837b73faaa70998b0
Stengauar <112621021+stangaan@users.noreply.github.com>
2022-10-29 21:32:25 +0200

    обьяснение и описание массива.

---
+++ Arreys/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] arrays = new  int[100];//так пишется массив int [] new int [... ]число массивов
+        int [] myArrays = new int[20];
+        int [] myTwoArrays = new int[50];
+        int [] yearArrays = new int[20]
+        System.out.println(Arrays.toString(arrays));
+    arrays[1] = 10000;//изминение конкретного массива= [ 1] на что меняем конкретный массив= 10000
+    myArrays[2] = 365;
+    myTwoArrays[49] =322;
+        System.out.println(Arrays.toString(myArrays));//так мы выводим массив в консоль.
+        System.out.println(Arrays.toString(arrays));
+        System.out.println(Arrays.toString(myTwoArrays));
+    }
+}
